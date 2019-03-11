b784d875bcd43062113d8e7f57e56231375ca109
david-mears <kieukmail@gmail.com>
2019-03-11 16:21:45 +0000

    implement openAccount() for Bank.prototype

--- spec/BankSpec.js
+++ spec/BankSpec.js
@@ -5,9 +5,27 @@ describe('Bank', function() {
 
   beforeEach(function() {
     bank = new Bank;
+    jasmine.clock().install;
+  });
+
+  afterEach(function() {
+    jasmine.clock().uninstall();
   });
 
   it('contains an array of accounts', function() {
     expect(bank.accounts.length).toEqual(0);
   });
+
+  it('creates a new account with #openAccount', function() {
+    const date = new Date(2017, 1, 1);
+    jasmine.clock().mockDate(date);
+    bank.openAccount();
+    bank.accounts[0].deposit(50)
+    expect(bank.accounts[0].history.length).toEqual(1);
+    expect(bank.accounts[0].history[0].date).toEqual(date);
+  })
+
+  it('displays the dates from the history of an account in reverse order', function() {
+
+  })
 });
--- src/Bank.js
+++ src/Bank.js
@@ -7,3 +7,8 @@ function Bank(accountConstructor = ClientAccount) {
   this.accounts = [];
   this.accountConstructor = accountConstructor;
 }
+
+Bank.prototype.openAccount = function(transactionConstructor = Transaction) {
+  let account = new this.accountConstructor(transactionConstructor);
+  this.accounts.push(account)
+}
