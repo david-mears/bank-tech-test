79202e63fcac55df8d191a0500151d5c6e72f2b4
david-mears <kieukmail@gmail.com>
2019-03-11 15:42:07 +0000

    remove jasmine sample files; implement ClientAccount deposit function with dependency injection

--- spec/ClientAccountSpec.js
+++ spec/ClientAccountSpec.js
@@ -14,4 +14,17 @@ describe('ClientAccount', function() {
   it('initializes with empty list of transactions', function() {
     expect(clientAccount.history).toEqual([])
   });
+
+  describe('#deposit', function() {
+    it('creates a new Transaction in history with correct data', function() {
+      let date1 = new Date(2000, 12, 25);
+      jasmine.clock().mockDate(date1);
+      clientAccount.deposit(33);
+      expect(clientAccount.history.length).toEqual(1)
+      let transaction = clientAccount.history[0];
+      expect(transaction.date).toEqual(date1);
+      expect(transaction.balanceChange).toEqual(33);
+    })
+  })
+
 });
--- src/ClientAccount.js
+++ src/ClientAccount.js
@@ -3,6 +3,12 @@
 /**
  * @constructor
  */
-function ClientAccount() {
-  this.history = []
+function ClientAccount(transactionConstructor = Transaction) {
+  this.history = [];
+  this.transactionConstructor = transactionConstructor;
+}
+
+ClientAccount.prototype.deposit = function(amount) {
+  let transaction = new this.transactionConstructor(amount);
+  this.history.push(transaction);
 }
