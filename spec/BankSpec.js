856015bf058c9425d155b63b202c0ead3de27a1f
david-mears <kieukmail@gmail.com>
2019-03-11 16:25:30 +0000

    refactor

--- spec/BankSpec.js
+++ spec/BankSpec.js
@@ -5,6 +5,7 @@ describe('Bank', function() {
 
   beforeEach(function() {
     bank = new Bank;
+    bank.openAccount();
     jasmine.clock().install;
   });
 
@@ -13,13 +14,13 @@ describe('Bank', function() {
   });
 
   it('contains an array of accounts', function() {
-    expect(bank.accounts.length).toEqual(0);
+    let newBank = new Bank;
+    expect(newBank.accounts.length).toEqual(0);
   });
 
   it('creates a new account with #openAccount', function() {
     const date = new Date(2017, 1, 1);
     jasmine.clock().mockDate(date);
-    bank.openAccount();
     bank.accounts[0].deposit(50);
     expect(bank.accounts[0].history.length).toEqual(1);
     expect(bank.accounts[0].history[0].date).toEqual(date);
